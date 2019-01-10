198df8fa66d517d4c6800855e9379d884261802b
T5wRzOtsUndPho <T5wRzOtsUndPho@mail.ru>
2019-01-10 22:59:00 +0300

    попытался сделать интеграцию React+Yii. чтобы посмотреть результат, нужно после аутентификации зайти на вкладку "проекты". пытался всунуть react в <div id="app"></div>

--- frontend/assets/DemandsAsset.php
+++ frontend/assets/DemandsAsset.php
@@ -7,14 +7,16 @@ use yii\web\AssetBundle;
 /**
  * Main frontend application asset bundle.
  */
-class AppAsset extends AssetBundle
+class DemandsAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/site.css','main.7e05470a43c7316545fc.css',
     ];
-    public $js = [];
+    public $js = [
+        'bundle.7e05470a43c7316545fc.js',
+    ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
